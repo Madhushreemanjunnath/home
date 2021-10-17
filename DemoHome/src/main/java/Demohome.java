e2da435b3f8aae2d00995637cf565f62e1bc989b
Manjunath <Manjunath@HP>
2021-10-17 11:59:13 +0530

    changes now

--- DemoHome/src/main/java/Demohome.java
+++ DemoHome/src/main/java/Demohome.java
@@ -4,5 +4,7 @@ public void main(String[] arg)
 {
 	System.out.println("Demohomeon sunday");
 	System.out.println("Demohomeon sunday");
+	System.out.println("unday");
+	System.out.println("sunday");
 }
 }

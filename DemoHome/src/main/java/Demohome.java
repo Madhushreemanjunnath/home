d71337201e774b7096aee6efe6206a91c6c4a2a2
Manjunath <Manjunath@HP>
2021-10-17 17:13:23 +0530

    check

--- DemoHome/src/main/java/Demohome.java
+++ DemoHome/src/main/java/Demohome.java
@@ -3,7 +3,7 @@ public class Demohome {
 public void main(String[] arg)
 {
 	System.out.println("Demohomeon sunday");
-	System.out.println("Demohomeon sunday");
+	System.out.println("hi");
 	System.out.println("plz help me in this ");
 	System.out.println("sunday");
 }
